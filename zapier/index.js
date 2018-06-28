e5f0e4f03685e255fc963c0e62d62eab694770cd
Mike Solomon <msolomon@thrillist.com>
2018-06-28 14:19:11 -0400

    send daily digest of new hires

--- zapier/index.js
+++ zapier/index.js
@@ -18,6 +18,25 @@ module.exports = {
       
     });
     
-  }
+  },
+  sendNewHiresStartingTodayWebhook: function(data) {
+    return new Promise(function(resolve, reject) {
+      
+      let options = {
+        method: 'post',
+        body: data,
+        json: true,
+        url: process.env.ZAPIER_TODAYS_NEW_HIRES_ENDPOINT
+      }
+      
+      request(options, function(err, response, body) {
+        if (err) { console.log(err); return reject(err); }
+        return resolve(body)
+      })
+      
+    });
+    
+  },
+  
   
 }
